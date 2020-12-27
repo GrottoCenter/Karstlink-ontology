c0f5c3d2c56746e4d9cf36f1e4911690c04e2a84
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-12-27 13:28:48 +0100

    prefixes.ttl : Turtle prefixes in use; conforming to http://prefix.cc depot

--- scripts/load-grotto-entrances.sh
+++ scripts/load-grotto-entrances.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # la boucle shell qui charge dans semantic_forms les /entrances/NNN via /load-uri?url=     et    /json2rdf?src=
 
 SERVER=http://localhost:9000
 API=https://beta.grottocenter.org/api/v1/entrances
-COUNT=3 # 80000
-OFFSET=10 # 1
+COUNT=10000 # 80000
+OFFSET=1795 # 1773 # 1628 # 1
 
 declare -i END
 END=$OFFSET+$COUNT
@@ -11,8 +13,10 @@ END=$OFFSET+$COUNT
 for (( item=$OFFSET; item<=$END; item+=1 ));
 do
   echo "======== Loading <$API/$item> ========"
-  curl \
+  # /usr/bin/time -f " Elapsed %E" curl \
+  time curl \
     --data-urlencode url=$SERVER/json2rdf?src=$API/$item \
     $SERVER/load-uri
-  sleep 5 ; echo
+  # sleep 1 ; 
+  # echo
 done
